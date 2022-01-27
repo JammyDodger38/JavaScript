68c3787fc539dfd4cbf8b08564dd79d90bafde6c
Vlad Vladimirov <blad20002000@mail.ru>
2022-01-27 17:08:26 +0700

    новый проект

---
+++ script.js
@@ -0,0 +1,10 @@
+// Переменные
+let title = "Заголовок"
+let screens = "Экраны"
+let screenPrice = "Экран с ценами"
+let rollback = "Откат"
+let fullPrice = "Полная стоимость"
+let adaptive = "Адаптивность"
+
+alert(screenPrice);
+console.log(rollback);
